08767e2cab41313d6c1a9ce88e02b72fb9ae7c27
Prophecy-Spellbook <134470979+Prophecy-Spellbook@users.noreply.github.com>
2023-06-14 13:02:39 +0900

    GPT's_example

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayInformation() {
+  var name = "John Doe";
+  var age = 30;
+  var occupation = "Web Developer";
+
+  document.getElementById("name").textContent = name;
+  document.getElementById("age").textContent = age;
+  document.getElementById("occupation").textContent = occupation;
+}
