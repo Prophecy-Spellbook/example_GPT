60df463e444853c31dbedd37d1657c34e93372be
Prophecy-Spellbook <134470979+Prophecy-Spellbook@users.noreply.github.com>
2023-07-08 17:36:57 +0900

    asdf

--- script.js
+++
@@ -1,9 +0,0 @@
-function displayInformation() {
-  var name = "사람";
-  var age = 30;
-  var occupation = "Web Developer";
-
-  document.getElementById("name").textContent = name;
-  document.getElementById("age").textContent = age;
-  document.getElementById("occupation").textContent = occupation;
-}
